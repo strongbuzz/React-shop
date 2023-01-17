61ac0fcd9926e0f6f61190435253651b1a4c5059
Jinwoo Lee <107897025+strongbuzz@users.noreply.github.com>
2023-01-17 06:51:27 -0500

    done catergory componetn

--- src/components/nav/CategorySelector.jsx
+++ src/components/nav/CategorySelector.jsx
@@ -1,9 +1,25 @@
 export default function CategorySelector() {
+
+    const title ="All"
+    const categories = ["All", "Jackets", "Scarfs", "Gloves", "Hats", "Socks"]
+
     return(
         <div className="dropdown mb-3 mb-lg-0">
-            <button className="btn btn-outline-success text-white dropdwon-toggle">
-                All
+            <button className="btn btn-outline-success text-white dropdwon-toggle"
+                    type="button"
+                    id ="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+            >
+                { title }
             </button>
+            <ul className="dropdown-menu">
+                {categories.map((category)=>(
+                    <li key={category}>
+                    <a href="#" className="dropdown-item pointer">{category}</a>
+                </li>
+                ))}
+                
+            </ul>
         </div>
     )
 }
