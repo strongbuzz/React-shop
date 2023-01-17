c59ffbfb2ce0adfb8a1abd02a2d28573130d64a2
Jinwoo Lee <107897025+strongbuzz@users.noreply.github.com>
2023-01-17 07:41:02 -0500

    test

---
+++ TestFile.jsx
@@ -0,0 +1,13 @@
+export default function SearchBar() {
+    return (
+        <form className="d-flex ms-md-0 ms-lg-3">
+            {" "}
+            <input
+                type="text"
+                placeholder="Search Products"
+                onChange={e => {}}
+                className="form-control ms-md-auto me-2"
+            />
+        </form>
+    );
+}
